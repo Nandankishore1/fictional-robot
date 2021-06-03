44a16e2bd32afc6dc7dbc249b43f9075952e6e44
nandan kishore <munganandankishore@gmail.com>
2021-06-03 21:58:54 +0530

    inital version of the program

---
+++ example1.py
@@ -0,0 +1,10 @@
+print("i will count my chickens")
+print("hens",25+30/6)
+print("roosters",100-25*3%4)
+print("now i will count the eggs:")
+print(3+2+1-5+4%2-1/4+6)
+print("is it true that 3+2<5-7?")
+print(3+2<5-7)
+print("what is 3+2?",3+2)
+print("oh, that is why it is false.")
+
